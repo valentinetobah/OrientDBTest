b8009acfe94fd09c70b998859d11cf510fe65ec8
Valentine Tobah <valentinetobah@gmail.com>
2017-09-06 18:59:55 +0200

    Done with orientdb rest crud

--- src/main/java/com/orientdbtest/dao/OrientDao.java
+++ src/main/java/com/orientdbtest/dao/OrientDao.java
@@ -14,20 +14,22 @@ import java.util.List;
 import com.orientdbtest.connection.OrientDBConnection;
 import com.orientdbtest.pojo.Student;
 import com.orientdbtest.pojo.Subject;
+import com.orientechnologies.orient.core.id.ORecordId;
 
 /**
  *
  * @author tobah
  */
-public class OrientDao {   
+public class OrientDao {
 
     public static List<Student> getAllStudents() {
-        
+
         List<Student> studentList = new ArrayList<Student>();
-        
+
         String select = "select * from student";
-        
+
         try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+            
             try (OResultSet rs = db.query(select)) {
 
                 while (rs.hasNext()) {
@@ -35,19 +37,14 @@ public class OrientDao {
                     OResult row = rs.next();
 
                     Student student = new Student();
-                    
+
                     student.setRid((row.getProperty("@rid")).toString());
                     student.setName((String) row.getProperty("name"));
                     student.setSurname((String) row.getProperty("surname"));
 
-                    System.out.println("\nRID: " + student.getRid().toString());
-                    System.out.println("Name: " + student.getName());
-                    System.out.println("Surname: " + student.getSurname());
-
                     studentList.add(student);
 
                 }
-                System.out.println("\nStudent List: " + studentList.size());
             }
         }
 
@@ -55,12 +52,13 @@ public class OrientDao {
     }
 
     public static List<Subject> getAllSubjects() {
-        
+
         List<Subject> subjectList = new ArrayList<Subject>();
 
         String select = "select * from subject";
 
         try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+            
             try (OResultSet rs = db.query(select)) {
 
                 while (rs.hasNext()) {
@@ -73,64 +71,265 @@ public class OrientDao {
                     subject.setTitle((String) row.getProperty("title"));
                     subject.setPercentageScore((int) row.getProperty("percentageScore"));
 
-                    System.out.println("\nRID: " + subject.getRid());
-                    System.out.println("Title: " + subject.getTitle());
-                    System.out.println("Percentage Score: " + subject.getPercentageScore());
-
                     subjectList.add(subject);
 
                 }
-                System.out.println("\nSubject List: " + subjectList.size());
             }
         }
 
         return subjectList;
     }
-    
-    public static Subject getSubject(String id){
+
+    public static Subject getSubject(String rid) {
+        
         List<Subject> subject = getAllSubjects();
-        for(Subject sub : subject){
-            if(sub.getRid().equalsIgnoreCase(id)){
+        
+        for (Subject sub : subject) {
+            
+            if (sub.getRid().equalsIgnoreCase(rid)) {
+                
                 return sub;
             }
         }
-        
+
         return null;
     }
-    
-    public static Student getStudent(String id){
+
+    public static Student getStudent(String rid) {
+        
         List<Student> student = getAllStudents();
-        for(Student stud : student){
-            if(stud.getRid().equalsIgnoreCase(id)){
+        
+        for (Student stud : student) {
+            
+            if (stud.getRid().equalsIgnoreCase(rid)) {
+                
                 return stud;
             }
         }
-        
+
         return null;
     }
-    
-    public static String addStudent(String name, String surname){
-        
-        String sql 
+
+    public static Student addStudent(String name, String surname) {
+
+        String sql
                 = "insert into Student content { name: ?, surname: ? }";
-        
-        OrientDBConnection.getConnection().query(sql, name, surname);
-        
-        System.out.println("New Student record added");
-        
-        return "You have Successfully added a new Student record";
-    }
-    
-    public static String addSubject(String title, int percentageScore){
-        String sql 
-                = "insert into Subject content { name: ?, surname: ? }";
-        
-        OrientDBConnection.getConnection().query(sql, title, percentageScore);
-        
-        System.out.println("New Subject record added");
-        
-        return "You have Successfully added a new Subject ecord";
+
+        try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+
+            try (OResultSet rs = db.query(sql, name, surname)) {
+
+                while (rs.hasNext()) {
+
+                    OResult row = rs.next();
+
+                    Student student = new Student();
+
+                    student.setRid((row.getProperty("@rid")).toString());
+                    student.setName((String) row.getProperty("name"));
+                    student.setSurname((String) row.getProperty("surname"));
+
+                    return student;
+
+                }
+            }
+        }
+
+        return null;
     }
 
+    public static Subject addSubject(String title, int percentageScore) {
+        String sql
+                = "insert into Subject content { title: ?, percentageScore: ? }";
+
+        try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+
+            try (OResultSet rs = db.query(sql, title, percentageScore)) {
+
+                while (rs.hasNext()) {
+
+                    OResult row = rs.next();
+
+                    Subject subject = new Subject();
+
+                    subject.setRid(row.getProperty("@rid").toString());
+                    subject.setTitle((String) row.getProperty("title"));
+                    subject.setPercentageScore((int) row.getProperty("percentageScore"));
+
+                    return subject;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static Student updateStudentName(String rid, String name) {
+
+        String sql = "select from Student where @rid = ?";
+
+        ORecordId orid = new ORecordId(rid);
+
+        try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+
+            try (OResultSet rs = db.query(sql, orid)) {
+
+                while (rs.hasNext()) {
+
+                    OResult row = rs.next();
+
+                    row.getVertex().ifPresent(x -> {
+                        x.setProperty("name", name);
+                        x.save();
+                    });
+                    
+                    Student student = new Student();
+
+                    student.setRid((row.getProperty("@rid")).toString());
+                    student.setName((String) row.getProperty("name"));
+                    student.setSurname((String) row.getProperty("surname"));
+
+                    return student;
+
+                }
+            }
+
+        }
+        return null;
+    }
+
+    public static Student updateStudentSurname(String rid, String surname) {
+
+        String sql = "select from Student where @rid = ?";
+
+        ORecordId orid = new ORecordId(rid);
+
+        try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+
+            try (OResultSet rs = db.query(sql, orid)) {
+
+                while (rs.hasNext()) {
+
+                    OResult row = rs.next();
+                    
+                    row.getVertex().ifPresent(x -> {
+                        x.setProperty("surname", surname);
+                        x.save();
+                    });
+
+                    Student student = new Student();
+
+                    student.setRid((row.getProperty("@rid")).toString());
+                    student.setName((String) row.getProperty("name"));
+                    student.setSurname((String) row.getProperty("surname"));
+
+                    return student;
+
+                }
+            }
+
+        }
+        return null;
+    }
+
+    public static Subject updateSubjectTitle(String rid, String title) {
+
+        String sql = "select from Subject where @rid = ?";
+
+        ORecordId orid = new ORecordId(rid);
+
+        try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+
+            try (OResultSet rs = db.query(sql, orid)) {
+
+                while (rs.hasNext()) {
+
+                    OResult row = rs.next();
+                    
+                    row.getVertex().ifPresent(x -> {
+                        x.setProperty("title", title);
+                        x.save();
+                    });
+
+                    Subject subject = new Subject();
+
+                    subject.setRid(row.getProperty("@rid").toString());
+                    subject.setTitle((String) row.getProperty("title"));
+                    subject.setPercentageScore((int) row.getProperty("percentageScore"));
+
+                    return subject;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static Subject updateSubjectScore(String rid, int score) {
+
+        String sql = "select from Subject where @rid = ?";
+
+        ORecordId orid = new ORecordId(rid);
+
+        try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+
+            try (OResultSet rs = db.query(sql, orid)) {
+
+                while (rs.hasNext()) {
+
+                    OResult row = rs.next();
+                    
+                    row.getVertex().ifPresent(x -> {
+                        x.setProperty("percentageScore", score);
+                        x.save();
+                    });
+
+                    Subject subject = new Subject();
+
+                    subject.setRid(row.getProperty("@rid").toString());
+                    subject.setTitle((String) row.getProperty("title"));
+                    subject.setPercentageScore((int) row.getProperty("percentageScore"));
+
+                    return subject;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static String deleteStudentById(String rid) {
+
+        String sql = "delete vertex Student where @rid = ?";
+
+        ORecordId orid = new ORecordId(rid);
+
+        try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+            
+            db.query(sql, orid);
+
+            System.out.println("Student record successfully deleted");
+
+            return "Student record with RID = " + rid + " has been successfully deleted";
+        }
+
+    }
+
+    public static String deleteSubjectById(String rid) {
+
+        String sql = "delete vertex Subject where @rid = ?";
+
+        ORecordId orid = new ORecordId(rid);
+
+        try (ODatabaseDocument db = OrientDBConnection.getConnection()) {
+            
+            db.query(sql, orid);
+
+            System.out.println("Subject record successfully deleted");
+
+            return "Subject record with RID = " + rid + " has been successfully deleted";
+        }
+
+    }
 
 }
--- src/main/java/com/orientdbtest/service/Service.java
+++ src/main/java/com/orientdbtest/service/Service.java
@@ -2,67 +2,170 @@ package com.orientdbtest.service;
 
 import com.google.gson.Gson;
 import com.orientdbtest.dao.OrientDao;
-import com.orientechnologies.orient.core.id.ORID;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
 
 @Path("service")
 public class Service {
- 
-   @GET
-   @Path("students")
-   @Produces(MediaType.APPLICATION_JSON)
-   public String getStudents(){
-       String  json = new Gson().toJson(OrientDao.getAllStudents());
-       return json;
-       
-   }
-   
-   @GET
-   @Path("subjects")
-   @Produces(MediaType.APPLICATION_JSON)
-   public String getSubjects(){
-       String  json = new Gson().toJson(OrientDao.getAllSubjects());
-       return json;
-       
-   }
-   
-   @GET
-   @Path("students/{rid}")
-   @Produces(MediaType.APPLICATION_JSON)
-   public String getStudent(@PathParam("rid") String rid){
-       String  json = new Gson().toJson(OrientDao.getStudent(rid));
-       return json;
-       
-   }
-   
-   @GET
-   @Path("subjects/{rid}")
-   @Produces(MediaType.APPLICATION_JSON)
-   public String getSubject(@PathParam("rid") String rid){
-       String  json = new Gson().toJson(OrientDao.getSubject(rid));
-       return json;
-   }
-       
-   @POST
-   @Path("addsubject/{title}/{percentageScore}")
-   @Produces(MediaType.APPLICATION_JSON)
-   public String addSubject(@PathParam("title") String title, 
-           @PathParam("percentageScore") int percentageScore){
-       return OrientDao.addSubject(title, percentageScore);
-       
-   }
-   
-   @POST
-   @Path("addstudent/{name}/{surname}")
-   @Produces(MediaType.APPLICATION_JSON)
-   public String addStudent(@PathParam("name") String name, 
-           @PathParam("percentageScore") String surname){
-       return OrientDao.addStudent(name, surname);
-   }
+
+    @GET
+    @Path("student")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getStudent(@QueryParam("rid") String rid) {
+        
+        String json;
+        String rid1 = "#" + rid;
+        
+        if (rid1 == null) {
+            
+            json = new Gson().toJson(OrientDao.getAllStudents());
+            
+            return json;
+            
+        } else {
+            
+            json = new Gson().toJson(OrientDao.getStudent(rid1));
+            
+            return json;
+        }
+    }
+
+    @GET
+    @Path("subject")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getSubject(@QueryParam("rid") String rid) {
+        
+        String json;
+        String rid1 = "#" + rid;
+        
+        if (rid1 == null) {
+            
+            json = new Gson().toJson(OrientDao.getAllSubjects());
+            
+        return json;
+        
+        } else {
+            
+            json = new Gson().toJson(OrientDao.getSubject(rid1));
+            
+            return json;
+        }
+    }
+
+    @POST
+    @Path("addsubject")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String addSubject(@QueryParam("title") String title,
+            @QueryParam("percentageScore") int percentageScore) {
+        
+        String json = new Gson()
+                .toJson(OrientDao.addSubject(title, percentageScore));
+        
+        if (json != null) {
+            
+            return json;
+        }
+        
+        return "No record was added";
+
+    }
+
+    @POST
+    @Path("addstudent")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String addStudent(@QueryParam("name") String name,
+            @QueryParam("surname") String surname) {
+        
+        String json = new Gson().toJson(OrientDao.addStudent(name, surname));
+        
+        if (json != null) {
+            
+            return json;
+        }
+        
+        return "No record was added";
+    }
+
+    @PUT
+    @Path("updatestudent")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateStudent(@QueryParam("rid") String rid,
+            @QueryParam("name") String name, @QueryParam("surname") String surname) {
+        
+        String rid1 = "#" + rid;
+        
+        if (rid1 != null) {
+            
+            if (name != null) {
+                
+                String json = new Gson()
+                        .toJson(OrientDao.updateStudentName(rid1, name));
+                
+                return json;
+            }
+            if (surname != null) {
+                
+                String json = new Gson()
+                        .toJson(OrientDao.updateStudentSurname(rid1, surname));
+                
+                return json;
+            }
+        }
+
+        return null;
+    }
+
+    @PUT
+    @Path("updatesubject")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateSubject(@QueryParam("rid") String rid,
+            @QueryParam("title") String title,
+            @QueryParam("score") String score) {
+        
+        String rid1 = "#" + rid;
+        
+        if (rid1 != null) {
+            
+            if (title != null) {
+                
+                String json = new Gson()
+                        .toJson(OrientDao.updateSubjectTitle(rid1, title));
+                
+                return json;
+            }
+            if (score != null) {
+                
+                int score1 = Integer.parseInt(score);
+                
+                String json = new Gson()
+                        .toJson(OrientDao.updateSubjectScore(rid1, score1));
+                
+                return json;
+            }
+        }
+
+        return null;
+    }
+
+    @DELETE
+    @Path("deletesubject")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String deleteSubject(@QueryParam("rid") String rid) {
+
+        return OrientDao.deleteSubjectById("#" + rid);
+    }
+
+    @DELETE
+    @Path("deletestudent")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String deleteStudent(@QueryParam("rid") String rid) {
+
+        return OrientDao.deleteStudentById("#" + rid);
+    }
 }
